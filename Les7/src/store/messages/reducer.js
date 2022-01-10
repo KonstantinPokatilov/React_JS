a248b261c4fb80aea0b8595d83460beaf0149fef
Konstantin Pokatilov <kotyara9494@list.ru>
2022-01-10 19:57:56 +1000

    7

---
+++ Les7/src/store/messages/reducer.js
@@ -0,0 +1,26 @@
+import { ADD_CHAT, DELETE_CHAT } from '../chats/actions';
+import { ADD_MESSAGE, } from './actions';
+
+const initialState = {};
+
+export const messagesReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_MESSAGE:
+            return {
+                ...state,
+                [action.payload.chatId]: [...state[action.payload.chatId], action.payload.message],
+            };
+        case ADD_CHAT:
+            return {
+                ...state,
+                [action.payload.id]: [],
+            };
+        case DELETE_CHAT: {
+            const newMessages = { ...state };
+            delete newMessages[action.payload]
+            return newMessages;
+        }
+        default:
+            return state;
+    }
+};
